25a0ef33c8059cb65cf29b8bc438ea2e6fbf5090
Alya Kovalenko <kovalenkoalinam@gmail.com>
2024-07-26 13:55:26 +0300

    feat: add webpack

---
+++ webpack.dev.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/, // Adding rule to handle CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'popup.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './public' }],
+    }),
+  ],
+};
