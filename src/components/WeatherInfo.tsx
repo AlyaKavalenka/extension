c6dcadaadcd39bcf137c53d6a3dfc0345419cffc
Alya Kovalenko <kovalenkoalinam@gmail.com>
2024-07-26 16:08:46 +0300

    feat: fetch weather

--- src/components/WeatherInfo.tsx
+++ src/components/WeatherInfo.tsx
@@ -1,9 +1,27 @@
-import { useAppSelector } from '../api/store/hooks'
+import { useEffect, useState } from 'react';
+import { useAppSelector } from '../api/store/hooks';
 
 export default function WeatherInfo() {
   const city = useAppSelector((state) => state.city.value);
 
-  return (
-    <h1>{city}</h1>
-  )
+  const [weather, setWeather] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(`https://wttr.in/${city}?0`);
+        const data = await response.text();
+        setWeather(data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        setWeather('Unable to fetch weather data.');
+      }
+    };
+
+    fetchWeather();
+  }, [city]);
+
+  console.log(weather);
+
+  return <div dangerouslySetInnerHTML={{ __html: weather || '' }}></div>;
 }
